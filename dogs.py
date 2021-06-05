cf8a7ce7a2831280b498d7132f8c5bd3e2779a15
Hoyci <ruan.pablo.pec@hotmail.com>
2021-06-05 09:28:57 -0300

    Atualizando para a seleção de subraças

--- dogs.py
+++ dogs.py
@@ -29,16 +29,33 @@ class DogsMail():
             if not verification.upper() == 'S':
                 break
 
-    def requisicao(self, raca):
-        self.raca = raca
-        self.res = requests.get(f'https://dog.ceo/api/breed/{self.raca}/images')
-        self.imgs = self.res.json()['message']
-        self.arquivo = self.imgs[randint(0, len(self.imgs) - 1)]
-        print(self.arquivo)
+    def requisicao(self, raca=None, subraca=None):
+        if raca==None and subraca==None:
+            self.res = requests.get(f'https://dog.ceo/api/breeds/image/random')
+            self.img = self.res.json()['message']
+            self.arquivo = self.img
+            print(self.arquivo)
+        elif subraca==None:
+            self.raca = raca
+            self.res = requests.get(f'https://dog.ceo/api/breed/{self.raca}/images')
+            self.imgs = self.res.json()['message']
+            self.arquivo = self.imgs[randint(0, len(self.imgs) - 1)]
+            print(self.arquivo)
+        else:
+            self.raca = raca
+            self.subraca = subraca
+            self.res = requests.get(f'https://dog.ceo/api/breed/{self.raca}/images').json()['message']
+            self.list = []
+            for i in self.res:
+                if f'{self.raca}-{self.subraca}' in i:
+                    self.list.append(i)
+            self.arquivo = self.list[randint(0, len(self.list) - 1)]
+            print(self.arquivo)
 
     def enviar(self):
         try:
-            message = MIMEText(f"<img src='{self.arquivo}' width='400' height='400'><br> Essa linda imagem foi enviada utilizando Python", 'html', 'utf-8')
+            message = MIMEText(f"""<img src='{self.arquivo}' width='400' height='400'><br> Essa linda imagem foi enviada utilizando Python.<br>
+                                Esse é um {self.raca}""", 'html', 'utf-8')
             message['subject'] = 'Um dog para você'
             message['from'] = self.username
             message['to'] = self.str_emails
@@ -51,5 +68,5 @@ class DogsMail():
             print('Erro ao enviar email', e)
 
 email = DogsMail(username='Seu email', password='Sua senha')
-email.requisicao(raca='A raça que você deseja. Ex: poodle')
+email.requisicao(raca='Uma raça', subraca='Uma subraca, caso tenha.')
 email.enviar()
